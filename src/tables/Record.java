202afe387d073ffd1c396e81078ff8d1619bfb59
Lisa Ketcham <lnk@udel.edu>
2016-04-22 12:48:47 -0400

    JavaDoc for Record and RecordTable
    created the Javadoc for classes and methods of Record and RecordTable

--- src/tables/Record.java
+++ src/tables/Record.java
@@ -6,6 +6,16 @@ import java.util.List;
 
 import javafx.scene.control.CheckBox;
 
+/**
+ * 
+ * Record holds the specific fields of one of the four types of Records depending on the given state.
+ * The state will determine what type of record, either Dance, Publication, Recording, or Album.
+ * The fields of this class must be all of the fields desired to be shown in the columns of the table,
+ * even if the fields are shared by multiple tables. The names of the fields should be identical to the
+ * fields of the schema in the table, must be final, and must be used, in this case, by a getter, whose 
+ * name must also match the format getField_name() for any field field_name.
+ * 
+ */
 public class Record {
 	
 	private final String barsperrepeat;
@@ -17,7 +27,8 @@ public class Record {
 	private CheckBox iHave;
 	private String index;
 
-	public Record(ResultSet set, String state, List<String> l) throws SQLException{
+	public Record(ResultSet set, String state) throws SQLException{
+		//Dance
 		if(state.equals("d")){
 			barsperrepeat =  set.getString("barsperrepeat");
 			name = set.getString("name");
@@ -26,6 +37,7 @@ public class Record {
 			devisor_id = "";
 			repetitions = ""; 
 		}
+		//Publication
 		else if(state.equals("p")){
 			name = set.getString("name");
 			devisor_id = set.getString("devisor_id");
@@ -34,6 +46,7 @@ public class Record {
 			type_id = "";
 			artist_id = "";
 		}
+		//Recording
 		else if(state.equals("r")){
 			name = set.getString("name");
 			type_id = set.getString("type_id");
@@ -42,6 +55,7 @@ public class Record {
 			artist_id="";
 			devisor_id = "";
 		}
+		//Album
 		else{
 			name = set.getString("name");
 			artist_id = set.getString("artist_id");
--- src/tables/RecordTable.java
+++ src/tables/RecordTable.java
@@ -2,18 +2,14 @@ package tables;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.List;
-
 import database.Database;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
@@ -24,6 +20,16 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.util.Callback;
 
+/**
+ * 
+ * Record Table holds information regarding the TableView and CellInfo of the passed in state and table.
+ * The four types of RecordTables would be a Dance Table, Publication Table, Recording Table and Album Table.
+ * The columns are created based on a mapping from column name to field name of that specific Record.
+ * Should any more columns be added to a specific table, you must add to the LinkedHashMap accordingly.
+ * The fields must also be added to Record and initialized for that state. The state is one of "d", "p", "r" 
+ * or "a" representing a Dance, Publication, Recording, or Album respectively.
+ *
+ */
 public class RecordTable {
 	
 	private Database db; 
@@ -33,6 +39,12 @@ public class RecordTable {
 	private String tableString;
 	private String state;
 	
+	/**
+	 * constructor for RecordTable creates a table of records according to either Dance, Publication, Recording, or Album.
+	 * @param tableString String of the desired table name of the schema. Should correspond with state.
+	 * @param state String that determines what type of RecordTable this should be. Should correspond with tableString.
+	 * @throws SQLException
+	 */
 	public RecordTable(String tableString, String state) throws SQLException {
 		this.tableString = tableString;
 		this.state = state;
@@ -41,11 +53,16 @@ public class RecordTable {
 		cellInfo = new VBox(10);
 		cellInfo.setVisible(false);
 		colNameField = new LinkedHashMap<String, String>();
-		mapColumnNameToId(state);
+		mapColumnNameToId();
 		initializeTable();
 	}
 	
-	public void mapColumnNameToId(String state){
+	/**
+	 * returns the mapping of the class' state of column names in the TableView to field 
+	 * names of the Record according to the state
+	 */
+	public void mapColumnNameToId(){
+		//Dance
 		if(state.equals("d")){
 			colNameField.put("Name", "name");
 			colNameField.put("Type", "type_id");
@@ -53,18 +70,21 @@ public class RecordTable {
 			colNameField.put("I Have", "iHave");
 			colNameField.put("Index", "index");
 		}
+		//Album
 		else if(state.equals("a")){
 			colNameField.put("Name", "name");
 			colNameField.put("Artist", "artist_id");
 			colNameField.put("I Have", "iHave");
 			colNameField.put("Index", "index");
 		}
+		//Publication
 		else if(state.equals("p")){
 			colNameField.put("Name", "name");
 			colNameField.put("Author", "devisor_id");
 			colNameField.put("I Have", "iHave");
 			colNameField.put("Index", "index");
 		}
+		//Recording
 		else{
 			colNameField.put("Name", "name");
 			colNameField.put("Type", "type_id");
@@ -74,11 +94,16 @@ public class RecordTable {
 		}
 	}
 
+	/**
+	 * sets up the initial table that holds all of the records. Columns are created according to the mapping
+	 * and each record is added based on the a database query, getting all records according to the state
+	 * @throws SQLException
+	 */
 	public void initializeTable() throws SQLException{
-		//Record Table
 		table.setEditable(false);
 		ResultSet set = db.searchTableByName(tableString, "");
 		
+		//set up columns
 		Iterator<String> i = colNameField.keySet().iterator();
 		while(i.hasNext()){
 			String colName = i.next();
@@ -88,14 +113,17 @@ public class RecordTable {
 			col.setCellValueFactory(new PropertyValueFactory<Record, String>(field));
 			table.getColumns().add(col);
 		}
-
+		
+		//add rows to table
 		table.setItems(populate(set));
 		
+		//link columns to get CellInfo for a given cell
 		final TableColumn<Record, String> RecordCol = (TableColumn<Record, String>) table.getColumns().get(0);
 		RecordCol.setCellFactory(new Callback<TableColumn<Record, String>, TableCell<Record, String>>() {
 			@Override
 		    public TableCell<Record, String> call(TableColumn<Record, String> col) {
-		        final TableCell<Record, String> cell = new TableCell<Record, String>() {
+		        //set linked cellInfo to be underlined and blue
+				final TableCell<Record, String> cell = new TableCell<Record, String>() {
 		            @Override
 		            public void updateItem(String firstName, boolean empty) {
 		                super.updateItem(firstName, empty);
@@ -108,7 +136,7 @@ public class RecordTable {
 		                }
 		            }
 		         };
-		         
+		         //on double click, show cellInfo
 		         cell.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
 		             @Override
 		             public void handle(MouseEvent event) {
@@ -126,24 +154,45 @@ public class RecordTable {
 		RecordCol.setStyle( "-fx-alignment: CENTER-LEFT;");
 		}
 	
+	/**
+	 * takes the given database query result set and adds each result to the rows of the table as a Record
+	 * according to what table and what state.
+	 * @param set ResultSet of the query to populate the database with
+	 * @return ObservableList of Records containing all Records to be added to the rows of the table
+	 * @throws SQLException
+	 */
 	public ObservableList<Record> populate(ResultSet set) throws SQLException{
 		ObservableList<Record> data = FXCollections.observableArrayList();
-		List<String> l = new ArrayList<String>(colNameField.values());
 		while(set.next()){
-			data.add(new Record(set,state,l));
+			data.add(new Record(set,state));
 		}
 		return data;
 	}
 	
+	/**
+	 * sets the given list to be the rows of the table
+	 * @param data ObservableList of Records that should be the rows of the table
+	 */
 	public void setTableData(ObservableList<Record> data){
 		table.setItems(data);
 	}
 	
+	/**
+	 * returns the TableView of Records of this class
+	 * @return TableView of Records of this class
+	 */
 	public TableView<Record> getTable(){
 		return table;
 	}
 	
+	/**
+	 * sets this class' cellInfo according to colName and id and 
+	 * creates a button for toggling between table and cellInfo
+	 * @param colName String column name to display
+	 * @param id String id of the column to display
+	 */
 	public void setCellInfo(String colName, String id){
+		//set up cell info
 		cellInfo.getChildren().clear();
 		cellInfo.setStyle(
 				"-fx-padding: 10;" +
@@ -158,6 +207,7 @@ public class RecordTable {
 		cellInfo.getChildren().add(l2);
 		cellInfo.getChildren().add(tableReturn);
 		
+		//return to table
 		tableReturn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent arg0) {
@@ -167,6 +217,10 @@ public class RecordTable {
 		});
 	}
 	
+	/**
+	 * returns the VBox CellInfo of this class
+	 * @return VBox CellInfo of this class
+	 */
 	public VBox getCellInfo(){
 		return cellInfo;
 	}
